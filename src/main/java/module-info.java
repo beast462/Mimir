a51024994870a148e33682295f3b8f3bb677a2a6
beast462 <binhmattroi@gmail.com>
2021-10-01 06:52:48 +0700

    add fontawesome 5 pack

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,10 +5,11 @@ module net.beast462.int2204.mimir {
     requires javafx.fxml;
 
     requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.fontawesome5;
     requires org.kordamp.bootstrapfx.core;
 
     opens net.beast462.int2204.mimir to javafx.fxml;
     exports net.beast462.int2204.mimir;
-    exports net.beast462.int2204.mimir.Application;
-    opens net.beast462.int2204.mimir.Application to javafx.fxml;
+    exports net.beast462.int2204.mimir.application;
+    opens net.beast462.int2204.mimir.application to javafx.fxml;
 }
