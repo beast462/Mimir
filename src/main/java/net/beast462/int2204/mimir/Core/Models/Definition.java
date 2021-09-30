2d18f93c223e6a3ddba63fbf3dd56b84ab21f547
beast462 <binhmattroi@gmail.com>
2021-09-30 16:18:06 +0700

    remove data annotation

--- src/main/java/net/beast462/int2204/mimir/Core/Models/Definition.java
+++ src/main/java/net/beast462/int2204/mimir/Core/Models/Definition.java
@@ -1,22 +1,13 @@
 package net.beast462.int2204.mimir.Core.Models;
 
-import net.beast462.int2204.mimir.Core.Annotations.SQL.Column;
-import net.beast462.int2204.mimir.Core.Annotations.SQL.Table;
-
-@Table(name = "definitions")
 public class Definition {
-    @Column(primary = true)
     public int id;
 
-    @Column
     public String definition;
 
-    @Column(name = "word_ref")
     public int wordRef;
 
-    @Column(name = "word_type")
     public int wordType;
 
-    @Column
     public String example;
 }
--- src/main/java/net/beast462/int2204/mimir/Core/Models/Word.java
+++ src/main/java/net/beast462/int2204/mimir/Core/Models/Word.java
@@ -1,16 +1,9 @@
 package net.beast462.int2204.mimir.Core.Models;
 
-import net.beast462.int2204.mimir.Core.Annotations.SQL.Column;
-import net.beast462.int2204.mimir.Core.Annotations.SQL.Table;
-
-@Table(name = "words")
 public class Word {
-    @Column(primary = true, unique = true)
     public int id;
 
-    @Column(type = Column.Types.Varchar, size = 45, unique = true)
     public String content;
 
-    @Column
     public String pronunciation;
 }
