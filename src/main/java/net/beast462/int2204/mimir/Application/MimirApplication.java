493969132d635743172383616161e73b27db2828
beast462 <binhmattroi@gmail.com>
2021-09-30 20:06:25 +0700

    add BootstrapFX CSS

--- src/main/java/net/beast462/int2204/mimir/Application/MimirApplication.java
+++ src/main/java/net/beast462/int2204/mimir/Application/MimirApplication.java
@@ -8,12 +8,14 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 import net.beast462.int2204.mimir.Main;
+import org.kordamp.bootstrapfx.BootstrapFX;
 
 public class MimirApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("root-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        scene.getStylesheets().add(BootstrapFX.bootstrapFXStylesheet());
         stage.setTitle("Mimir dictionary");
         stage.setScene(scene);
         stage.show();
