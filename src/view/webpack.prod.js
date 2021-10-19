e05cc06aaede750f0d5c6fe7bdfd553aefb9ab84
beast462 <binhmattroi@gmail.com>
2021-10-19 22:40:43 +0700

    frontend build script

--- src/view/webpack.prod.js
+++ src/view/webpack.prod.js
@@ -1,48 +1,60 @@
 /* eslint-disable */
-const path = require("path");
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
 const context = __dirname;
 
 const common = {
   resolve: {
-    extensions: [".jsx", ".js"],
-    modules: [path.join(context, "src"), path.join(context, "node_modules")],
+    extensions: ['.jsx', '.js'],
+    modules: [path.join(context, 'src'), path.join(context, 'node_modules')],
+    alias: {
+      '~': path.resolve(__dirname, 'src'),
+      '@store': path.resolve(__dirname, 'src', 'store'),
+      '@components': path.resolve(__dirname, 'src', 'components'),
+      '@common': path.resolve(__dirname, 'src', 'common'),
+    },
   },
   node: {
     __dirname: false,
     __filename: false,
   },
   context,
-  mode: "production",
-  devtool: "none",
+  mode: 'production',
+  devtool: 'none',
 };
 
 module.exports = () => [
   {
-    entry: path.join(context, "src", "index.js"),
+    entry: path.join(context, 'src', 'index.js'),
     output: {
-      publicPath: "/dist/",
-      path: path.join(context, "dist"),
-      filename: "view.bundle.js",
+      publicPath: '/dist/',
+      path: path.join(context, 'dist'),
+      filename: 'view.bundle.js',
     },
-    target: "web",
+    target: 'web',
     module: {
       rules: [
         {
           test: /\.jsx?$/,
           exclude: /node_modules/,
-          use: ["babel-loader"],
+          use: ['babel-loader'],
         },
         {
           test: /\.(png|jpg|gif|webp|woff|woff2)$/,
-          use: ["file-loader"],
+          use: ['file-loader'],
         },
         {
           test: /\.css$/,
-          use: ["style-loader", "css-loader"],
+          use: ['style-loader', 'css-loader'],
         },
       ],
     },
-    plugins: [],
+    plugins: [
+      new HTMLWebpackPlugin({
+        filename: 'index.html',
+        template: path.join(context, 'public', 'index.html'),
+      }),
+    ],
     ...common,
   },
 ];
